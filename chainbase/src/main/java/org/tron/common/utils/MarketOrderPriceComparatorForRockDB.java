2260648f9dcacdc46e5824fcf1ca15793d17853e
Rocky Feng <folowing@gmail.com>
2022-08-26 21:26:27 +0800

    m1 build

--- chainbase/src/main/java/org/tron/common/utils/MarketOrderPriceComparatorForRockDB.java
+++ chainbase/src/main/java/org/tron/common/utils/MarketOrderPriceComparatorForRockDB.java
@@ -2,10 +2,12 @@ package org.tron.common.utils;
 
 import org.rocksdb.ComparatorOptions;
 import org.rocksdb.DirectSlice;
-import org.rocksdb.util.DirectBytewiseComparator;
+import org.rocksdb.AbstractComparator;
 import org.tron.core.capsule.utils.MarketUtils;
 
-public class MarketOrderPriceComparatorForRockDB extends DirectBytewiseComparator {
+import java.nio.ByteBuffer;
+
+public class MarketOrderPriceComparatorForRockDB extends AbstractComparator {
 
   public MarketOrderPriceComparatorForRockDB(final ComparatorOptions copt) {
     super(copt);
@@ -17,20 +19,16 @@ public class MarketOrderPriceComparatorForRockDB extends DirectBytewiseComparato
   }
 
   @Override
-  public int compare(final DirectSlice a, final DirectSlice b) {
+  public int compare(ByteBuffer a, ByteBuffer b) {
     return MarketUtils.comparePriceKey(convertDataToBytes(a), convertDataToBytes(b));
   }
 
   /**
    * DirectSlice.data().array will throw UnsupportedOperationException.
    * */
-  public byte[] convertDataToBytes(DirectSlice directSlice) {
-    int capacity = directSlice.data().capacity();
-    byte[] bytes = new byte[capacity];
-
-    for (int i = 0; i < capacity; i++) {
-      bytes[i] = directSlice.get(i);
-    }
+  public byte[] convertDataToBytes(ByteBuffer buf) {
+    byte[] bytes = new byte[buf.remaining()];
+    buf.get(bytes);
 
     return bytes;
   }
